0170e95d8bec3d9eed82c937688c875a3a7e9d95
ahrionn <luam_azf@hotmail.com>
2023-11-27 16:03:52 -0300

    trabalho de docker (eng de soft II)

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
